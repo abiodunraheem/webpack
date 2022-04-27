c6a1e9f50c9ed5b4771c2c8030fd1d2533441c1d
abiodunraheem <raheemabiodun23@gmail.com>
2022-04-27 12:25:49 +0100

    Add npm packages

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+export const mode = 'production';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
