7a127d08729c76e296e9f62c80744459ff83d99b
abiodunraheem <raheemabiodun23@gmail.com>
2022-04-27 16:59:26 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
-export const mode = 'production';
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          }
+        ]
+      },
+  mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html")
     })
-  ]
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  }
 };
